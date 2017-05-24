4e7cb19f36b65a250779ddac3bee8100714f36b1
EDLyonhart <EDLyonhart@gmail.com>
2017-05-24 11:15:25 -0700

    generate table of contents and stitch together with entire doc

--- test3/test.rb
+++ test3/test.rb
@@ -8,15 +8,20 @@ require 'combine_pdf'
 require 'prawn'
 require 'prawn/table'
 
-pdf = CombinePDF.new
+# arguments to locate all applicable files
 client = ARGV[0]
 address = ARGV[1]
+dir = "./#{client}/#{address}"
+
+# initialize two .pdf files for later use
+table_of_contents = Prawn::Document.new
+final_contract = CombinePDF.new
 
 file_names, files_to_load = Array.new, Array.new
-dir = "./#{client}/#{address}"
 
 # ------------
 # find every file in named directory. format with path and pdf file name
+# insert into array 'files_to_load'
 # ------------
 def generate_file_path(dir, files_to_load)
   Dir.foreach(dir) do |fname|
@@ -27,77 +32,51 @@ def generate_file_path(dir, files_to_load)
 end
 
 # ------------
-# create table of contents
+# get all file names
+# insert into array file_names
 # ------------
-def generate_table_of_contents(file_names)
-  Prawn::Document.generate("table_of_contents", page_layout: :portrait) do
-    file_names.each do |file|
-      text "<link anchor=#{file}>#{file}</link>", inline_format: true
-    end
-    start_new_page
-
-    file_names.each do |file|
-
-      #
-      # figure out how to get body. in examnple it is being generated on the spot. in my case, it needs to be loaded from 'files_to_load' array
-      #
-      
-      table body do |t|
-        t.before_rendering_page do |cells|
-          cells.each do |cell|
-            if cell.content == file
-              add_dest(cell.content, dest_xyz(bounds.absolute_left, y))
-            end
-          end
-        end
-      end
-    end
-  end
-
-  # Prawn::Document.generate("linked.pdf", page_layout: :landscape) do
-  #   text '<link anchor="hello30">Click me</link>', inline_format: true
-  #   start_new_page
-
-  #   body = (1..50).map do |i|
-  #     ["hello#{i}", "some info about hello#{i}"]
-  #   end
-
-  #   table body do |t|
-  #     t.before_rendering_page do |cells|
-  #       cells.each do |cell|
-  #         if cell.content == "hello30"
-  #           add_dest(cell.content, dest_xyz(bounds.absolute_left, y))
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
-end
-
 def get_file_names(dir, file_names)
   Dir.foreach(dir) do |fname|
     unless fname == '.' || fname == '..'
       file_names << fname
     end
   end
-
-  generate_table_of_contents(file_names)
 end
 
 # ------------
-# add each file to pdf
+# create table of contents
+# - create hash out of files
+#   - Index + 1
+#   - Name of file - '.pdf'
 # ------------
-def create_final_contract(file_names, files_to_load)
-  pdf << generate_table_of_contents(file_names)
-  files_to_load.each do |file|
-    pdf << CombinePDF.load(file)
+def generate_table_of_contents(table_of_contents, file_names)
+
+  contents_hash = Hash.new
+  file_names.each_with_index do |val,index|
+    contents_hash[index+1] = val[0..-5]
   end
+
+  table_of_contents.table(contents_hash) do |table|
+    table.rows(file_names.length)
+  end
+
+  table_of_contents.render_file "table_of_contents.pdf"
 end
 
-#
-# output combined pdf
-#
+
+# ------------
+# add table of contents and each file to pdf
+# ------------
+def create_final_contract(address, final_contract, files_to_load, table_of_contents)
+  files_to_load.unshift("table_of_contents.pdf")
+  files_to_load.each do |file|
+    final_contract << CombinePDF.load(file)
+  end
+
+  final_contract.save "#{address}_final_contract.pdf"
+end
+
 get_file_names(dir, file_names)
 generate_file_path(dir, files_to_load)
-create_final_contract(file_names, files_to_load)
-pdf.save "combined.pdf"
+generate_table_of_contents(table_of_contents, file_names)
+create_final_contract(address, final_contract, files_to_load, table_of_contents)
